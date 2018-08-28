88861ec3190aefa768d7eb127521259cddb231f8
Rana Ejtehadian <rana.ejtehadian@animalcharityevaluators.org>
2018-08-28 15:49:52 +0100

    Clean up

--- backend/api/controllers/RestaurantsController.js
+++ backend/api/controllers/RestaurantsController.js
@@ -6,13 +6,4 @@
  */
 
 module.exports = {
-	restaurants: function (req, res) { 
-		sails.log.silly('restaurants');
-		sails.log.silly('restaurants', req.allParams());
-		sails.log.silly(req.query.is_favorite);
-	},
-	is_favorite: function (req, res) { 
-		sails.log.silly('is_favorite');
-		res.status(200); res.jsonx({result: "OK"}); 
-	},
 };
--- backend/api/controllers/restaurants/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function welcomeUser (req, res) {
-		sails.log.silly('restaurants');
-		sails.log.silly('restaurants', req.allParams());
-		sails.log.silly(req.query.is_favorite);
-};
